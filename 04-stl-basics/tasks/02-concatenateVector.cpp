5eb881e63b1e79d2db6edd82f873f486a61033dc
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-14 20:23:00 +0200

    Solution for tasks 1-4 STL

--- 04-stl-basics/tasks/02-concatenateVector.cpp
+++ 04-stl-basics/tasks/02-concatenateVector.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <vector>
 
-// Implement concatenateVector.
-// It should take 2 vectors and return one which has alternately elements from the first and the other one.
+std::vector<int> concatenateVector(const std::vector<int>& vec1, const std::vector<int>& vec2) 
+{
+    std::vector<int> resultVector {};
+    int counter {0};
+
+    if (vec1.size() <= vec2.size()) {
+        for (auto el:vec1) {
+            resultVector.push_back(el);
+            resultVector.push_back(vec2.at(counter));
+            counter++;
+        }
+    } else {
+        for (auto el:vec2) {
+            resultVector.push_back(vec1.at(counter));
+            resultVector.push_back(el);
+            counter++;
+        }
+    }
+    return resultVector;
+}
 
 int main() {
-    std::vector<int> vec1 {1, 2, 3, 4, 5};
+    std::vector<int> vec1 {1, 2, 3, 4, 5, 6, 7};
     std::vector<int> vec2 {11, 12, 13, 14, 15};
 
     auto vec = concatenateVector(vec1, vec2);
--- 04-stl-basics/tasks/03-createSortedList.cpp
+++ 04-stl-basics/tasks/03-createSortedList.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
-// Implement createSortedList
-// add proper include :)
+std::list<int> createSortedList(const std::vector<int>& vec) {
+    
+    std::list<int> newList {};
+
+    for (auto el:vec) {
+        newList.push_back(el);
+    };
+    newList.sort();
+    return newList;
+}
 
 int main() {
     std::vector<int> vec{2, 3, 4, 1, 6, 5, 8, 7, 9, 0};
